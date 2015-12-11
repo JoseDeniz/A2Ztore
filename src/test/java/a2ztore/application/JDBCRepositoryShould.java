0b780a1f4a96f97537f1fbe2c78cf653a846d9f2
Jose Manuel Deniz Suarez <josema13992@gmail.com>
2015-12-11 18:17:22 +0000

    TODO List created for JDBC Repository

---
+++ src/test/java/a2ztore/application/JDBCRepositoryShould.java
@@ -0,0 +1,14 @@
+package a2ztore.application;
+
+public class JDBCRepositoryShould {
+
+    /**
+     * TODO List:
+     *
+     * - Add a person
+     * - Get a person
+     * - Update a person
+     * - Remove a person
+     */
+
+}
