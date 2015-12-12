29b5fa31ca8957471b3942430b5891ced7617fda
Jose Manuel Deniz Suarez <josema13992@gmail.com>
2015-12-12 19:11:03 +0000

    Refactor: clean production code

--- src/main/java/a2ztore/application/JDBCRepository.java
+++ src/main/java/a2ztore/application/JDBCRepository.java
@@ -4,17 +4,12 @@ import a2ztore.model.Person;
 import a2ztore.view.Repository;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-import static java.sql.DriverManager.getConnection;
+import static a2ztore.application.DatabaseConnector.stablishConnection;
 
 public class JDBCRepository implements Repository {
-
-    public static final String DB_URL = "jdbc:mysql://localhost:3306/a2ztoreDB";
-    public static final String USERNAME = "root";
-    public static final String PASSWORD = "secret";
 
     public JDBCRepository() {
         createUsersTable();
@@ -23,7 +18,7 @@ public class JDBCRepository implements Repository {
     @Override
     public void add(Person person) {
         String sql = "INSERT INTO Users (username, fullname) VALUES (?, ?)";
-        try (Connection connection = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD)) {
+        try (Connection connection = stablishConnection()) {
             if (connection != null) {
                 PreparedStatement statement = connection.prepareStatement(sql);
                 statement.setString(1, person.name());
@@ -36,12 +31,14 @@ public class JDBCRepository implements Repository {
     }
 
     private void createUsersTable() {
-        try (Connection connection = getConnection(DB_URL, USERNAME, PASSWORD)) {
+        try (Connection connection = stablishConnection()) {
             String sql = "CREATE TABLE Users (user_id int(11) NOT NULL AUTO_INCREMENT," +
                                              "username varchar(45) NOT NULL," +
                                              "fullname varchar(45) NOT NULL," +
                                              "PRIMARY KEY (user_id));";
-            connection.createStatement().executeUpdate(sql);
+            if (connection != null)
+                connection.createStatement().executeUpdate(sql);
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
